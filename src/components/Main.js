0f45ff2cf53351b4c6782a562199fb7283451012
Noor Mohammed <mail@noormohammed.tech>
2021-12-09 19:13:10 +0530

    fixed counting

--- src/components/Main.js
+++ src/components/Main.js
@@ -13,42 +13,43 @@ class Main extends Component {
               <p>&nbsp;</p>
               <div className="card mb-3 mx-auto bg-dark" style={{ maxWidth: '512px' }}>
                 <h2 className="text-white text-monospace bg-dark"><b><ins>Share File</ins></b></h2>
-                  <form onSubmit={(event) => {
-                    event.preventDefault()
-                    const description = this.fileDescription.value
-                    this.props.uploadFile(description)
-                  }} >
-                      <div className="form-group">
-                        <br></br>
-                          <input
-                            id="fileDescription"
-                            type="text"
-                            ref={(input) => { this.fileDescription = input }}
-                            className="form-control text-monospace"
-                            placeholder="description..."
-                            required />
-                      </div>
-                    <input type="file" onChange={this.props.captureFile} className="text-white text-monospace"/>
-                    <button type="submit" className="btn-primary btn-block"><b>Upload!</b></button>
-                  </form>
+                <form onSubmit={(event) => {
+                  event.preventDefault()
+                  const description = this.fileDescription.value
+                  this.props.uploadFile(description)
+                }} >
+                  <div className="form-group">
+                    <br></br>
+                    <input
+                      id="fileDescription"
+                      type="text"
+                      ref={(input) => { this.fileDescription = input }}
+                      className="form-control text-monospace"
+                      placeholder="description..."
+                      required />
+                  </div>
+                  <input type="file" onChange={this.props.captureFile} className="text-white text-monospace" />
+                  <button type="submit" className="btn-primary btn-block"><b>Upload!</b></button>
+                </form>
               </div>
               <p>&nbsp;</p>
-              <table className="table-sm table-bordered text-monospace" style={{ width: '1000px', maxHeight: '450px'}}>
+              <table className="table-sm table-bordered text-monospace" style={{ width: '1000px', maxHeight: '450px' }}>
                 <thead style={{ 'fontSize': '15px' }}>
                   <tr className="bg-dark text-white">
-                    <th scope="col" style={{ width: '10px'}}>id</th>
-                    <th scope="col" style={{ width: '200px'}}>name</th>
-                    <th scope="col" style={{ width: '230px'}}>description</th>
-                    <th scope="col" style={{ width: '120px'}}>type</th>
-                    <th scope="col" style={{ width: '90px'}}>size</th>
-                    <th scope="col" style={{ width: '90px'}}>date</th>
-                    <th scope="col" style={{ width: '120px'}}>uploader/view</th>
-                    <th scope="col" style={{ width: '120px'}}>hash/view/get</th>
-                    <th scope="col" style={{ width: '120px'}}>Upvotes</th>
+                    <th scope="col" style={{ width: '10px' }}>id</th>
+                    <th scope="col" style={{ width: '200px' }}>name</th>
+                    <th scope="col" style={{ width: '230px' }}>description</th>
+                    <th scope="col" style={{ width: '120px' }}>type</th>
+                    <th scope="col" style={{ width: '90px' }}>size</th>
+                    <th scope="col" style={{ width: '90px' }}>date</th>
+                    <th scope="col" style={{ width: '120px' }}>uploader/view</th>
+                    <th scope="col" style={{ width: '120px' }}>hash/view/get</th>
+                    <th scope="col" style={{ width: '120px' }}>Upvotes</th>
                   </tr>
                 </thead>
-                { this.props.files.map((file, key) => {
-                  return(
+                {this.props.files.map((file, key) => {
+                  // console.log("map working");
+                  return (
                     <thead style={{ 'fontSize': '12px' }} key={key}>
                       <tr>
                         <td>{file.fileId}</td>
@@ -62,21 +63,23 @@ class Main extends Component {
                             href={"https://etherscan.io/address/" + file.uploader}
                             rel="noopener noreferrer"
                             target="_blank">
-                            {file.uploader.substring(0,10)}...
+                            {file.uploader.substring(0, 10)}...
                           </a>
-                         </td>
+                        </td>
                         <td>
                           <a
                             href={"https://ipfs.infura.io/ipfs/" + file.fileHash}
                             rel="noopener noreferrer"
                             target="_blank">
-                            {file.fileHash.substring(0,10)}...
+                            {file.fileHash.substring(0, 10)}...
                           </a>
                         </td>
                         <td>
-                          <button onClick={() => this.props.increaseUpvotes(key+1)}>+</button>
-                          <span style={{ padding:"2px"}}>{file.upvotes}</span>
-                          <button onClick={() => this.props.decreaseUpvotes(key+1)}>-</button>
+                          {console.log(this.props.files.length)}
+                          {console.log(this.props.files)}
+                          <button onClick={() => this.props.increaseUpvotes(file.fileId)}>+</button>
+                          <span style={{ padding: "2px" }}>{file.upvotes}</span>
+                          <button onClick={() => this.props.decreaseUpvotes(file.fileId)}>-</button>
                         </td>
                       </tr>
                     </thead>
